5132a5509d0ef559fea15591f3c997cf304f9194
Helbak <helbak77@gmail.com>
2019-06-19 21:58:15 +0300

    added: - in Model variables+setters+getters from STEP1; - in Model string STEP2; - in Controller bth from STEP1 + listeners + function of change pages/

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -7,18 +7,51 @@ Controller.prototype.init = function () {
 const indev = document.getElementById('indev');
 indev.innerHTML = this.model.getStep1();
 
-const nextFromStep1 = document.getElementById("nextButton");
-    nextFromStep1.addEventListener('click', function () {
-        indev.innerHTML = this.model.getStep3();
-            const backButtonFrom3 = document.getElementById("backButton");
-            backButtonFrom3.addEventListener('click', function () {
-                    indev.innerHTML = this.model.getStep1();
-                }.bind(this),
-                false);
-
+const username = document.getElementById("username");
+username.addEventListener('click', function () {
+        this.model.setUsername(username.value);
     }.bind(this),
     false);
 
+    const password = document.getElementById("password");
+    password.addEventListener('click', function () {
+            this.model.setPassword(password.value);
+        }.bind(this),
+        false);
+
+    const confirm = document.getElementById("confirm");
+    confirm.addEventListener('click', function () {
+            this.model.setConfirm(confirm.value);
+        }.bind(this),
+        false);
+
+    const email = document.getElementById("email");
+    confirm.addEventListener('click', function () {
+            this.model.setEmail(email.value);
+        }.bind(this),
+        false);
+
+    const phone = document.getElementById("phone");
+    confirm.addEventListener('click', function () {
+            this.model.setPhone(phone.value);
+        }.bind(this),
+        false);
+
+    const nextFromStep1 = document.getElementById("nextButton");
+    nextFromStep1.addEventListener('click', function () {
+        indev.innerHTML = this.model.getStep2();
+            const backButtonFrom2 = document.getElementById("backButtonFrom2");
+            backButtonFrom2.addEventListener('click', function () {
+                    indev.innerHTML = this.model.getStep1();
+                }.bind(this),
+                false);
+            const nextButtonFrom2 = document.getElementById('nextButtonFrom2');
+            nextButtonFrom2.addEventListener('click', function () {
+                    indev.innerHTML = this.model.getStep3();
+                }.bind(this),
+                false);
 
 
+        }.bind(this),
+    false);
 };
--- src/model/model.js
+++ src/model/model.js
@@ -1,23 +1,56 @@
 function Model() {
-this.stringStep1 = " <div class=\"body__steps\">Step 1 of 3</div>\n" +
+    this.username = "";
+    this.password = "";
+    this.confirm = "";
+    this.phone = "";
+    this.email = "";
+this.stringStep1 = "<div class=\"body__steps\">Step 1 of 3</div>\n" +
     "        <div class=\"body__title\">Create an Account</div>\n" +
     "        <div class=\"body__step1\">\n" +
     "            <span  class=\"step1 span\">Username*</span>\n" +
-    "            <input class=\"step1 input\" type=\"text\">\n" +
+    "            <input class=\"step1 input\" type=\"text\" id = \"username\">\n" +
     "            <span  class=\"step1 span\">Password*</span>\n" +
-    "            <input class=\"step1 input\" type=\"text\">\n" +
+    "            <input class=\"step1 input\" type=\"text\" id = \"password\">\n" +
     "            <span  class=\"step1 span\">Confirm*</span>\n" +
-    "            <input class=\"step1 input\" type=\"text\">\n" +
+    "            <input class=\"step1 input\" type=\"text\" id = \"confirm\">\n" +
     "            <span  class=\"step1 span\">E-mail*</span>\n" +
-    "            <input class=\"step1 input\" type=\"text\">\n" +
+    "            <input class=\"step1 input\" type=\"text\" id = \"email\">\n" +
     "            <span  class=\"step1 span\">Phone*</span>\n" +
-    "            <input class=\"step1 input\" type=\"tel\">\n" +
+    "            <input class=\"step1 input\" type=\"tel\" id = \"phone\">\n" +
     "        </div>\n" +
     "        <div class=\"body__next\">\n" +
     "            <button class=\"next\" id=\"nextButton\">Next</button>\n" +
     "        </div>";
 
-    this.stringStep3 = "<div class=\"body__steps\">Step 3 of 3</div>\n" +
+    this.stringStep2 = "<div class=\"body__steps\">Step 2 of 3</div>-->\n" +
+        "<div class=\"body__title\">Create an Account</div>\n" +
+        "<div class=\"body__step1\">\n" +
+        "    <span class=\"step1 span\">Name*</span>\n" +
+        "    <input class=\"step1 input\" type=\"text\" id=\"name\">\n" +
+        "    <span class=\"step1 span\">Surname</span>\n" +
+        "    <input class=\"step1 input\" type=\"text\" id=\"surname\">\n" +
+        "\n" +
+        "\n" +
+        "    <span class=\"step1 span\">Gender</span>\n" +
+        "    <input class=\"step1 input\" type=\"text\" id=\"gender\" list=\"l2\">\n" +
+        "    <datalist id=\"l2\">\n" +
+        "        <option>0 - male</option>\n" +
+        "        <option>1 - female</option>\n" +
+        "    </datalist>\n" +
+        "\n" +
+        "\n" +
+        "    <span class=\"step1 span\">Relationship status</span>\n" +
+        "    <input class=\"step1 input\" type=\"text\" id=\"relation\">\n" +
+        "\n" +
+        "</div>\n" +
+        "<div class=\"body__back\">\n" +
+        "    <button class=\"back\" id=\"backButtonFrom2\">Back</button>\n" +
+        "</div>\n" +
+        "<div class=\"body__next\">\n" +
+        "    <button class=\"next\" id=\"nextButtonFrom2\">Next</button>\n" +
+        "</div>";
+
+        this.stringStep3 = "<div class=\"body__steps\">Step 3 of 3</div>\n" +
         "\n" +
         "    <div class=\"body__step1\">\n" +
         "        <span  class=\"step1 span\">Programming languages</span>\n" +
@@ -47,7 +80,7 @@ this.stringStep1 = " <div class=\"body__steps\">Step 1 of 3</div>\n" +
         "        </datalist>\n" +
         "        <div class=\"step1 checkbox\">\n" +
         "            <input type=\"checkbox\">\n" +
-        "            <label for=\"checkbox\">Accept the <a href=\"http://www.yandex.ru\">registration</a> rules</label>\n" +
+        "            <label for=\"checkbox\">Accept the <a href=\"http://mock.html\">registration</a> rules</label>\n" +
         "        </div>\n" +
         "\n" +
         "\n" +
@@ -63,6 +96,47 @@ this.stringStep1 = " <div class=\"body__steps\">Step 1 of 3</div>\n" +
 Model.prototype.getStep1 = function(){
     return this.stringStep1;
 };
+Model.prototype.getStep2 = function(){
+    return this.stringStep2;
+};
 Model.prototype.getStep3 = function(){
     return this.stringStep3;
 };
+Model.prototype.getUsername = function(){
+    return this.username;
+};
+Model.prototype.setUsername = function(username){
+    return this.username = username;
+};
+
+Model.prototype.getPassword = function(){
+    return this.password;
+};
+Model.prototype.setPassword = function(password){
+    return this.password = password;
+};
+
+Model.prototype.getConfirm = function(){
+    return this.confirm;
+};
+Model.prototype.setConfirm = function(confirm){
+    return this.confirm = confirm;
+};
+
+Model.prototype.getPhone = function(){
+    return this.phone;
+};
+Model.prototype.setPhone = function(phone){
+    return this.phone = phone;
+};
+
+Model.prototype.getEmail = function(){
+    return this.email;
+};
+Model.prototype.setEmail = function(email){
+    return this.email = email;
+};
+
+
+
+
